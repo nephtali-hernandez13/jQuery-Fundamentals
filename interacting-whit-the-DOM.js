30843ca2abb1e5b7da01a5bc0294ec7a745e85a5
Nephtali Abisai Hernández Olvera <sistemas.naho13@gmail.com>
2019-12-17 09:58:09 -0700

    Add interacting whit the DOM

---
+++ interacting-whit-the-DOM.js
@@ -0,0 +1,59 @@
+$(document).ready(() => {
+
+    /*
+    // Overwrite DOM
+    var div3 = $(".classXD2");    
+    $('.classXD').each( index => {                
+        div3.html("Pasando por el DIV # " + index + " Y el DIV # " + (index - 1));
+    } );
+    */
+    /*
+    // Add title attribute
+    var val = $(".classXD2");
+    val.attr('title','My Title');
+    $('.classXD').each( index => {
+        $('#div'+index).attr('title', 'My title ' + index);        
+    });
+
+    //$(".classXD2").css("font-size", "29px");
+    $(".classXD2").attr({
+        title: 'Some title 3',
+        style: 'font-size: 18pt; background: yellow'
+    })
+    .css("color",'blue');
+    */
+
+    /*
+    //Using Append & AppendTo
+    // append & appendTo <-- start
+    $("#lista").append("<li>Item</li>");
+    $("<li>xDD</li>").appendTo('#lista');
+
+    //Using Prepend & PrependTo
+    // prepend & prependTo <-- end
+    $("#lista").prepend("<li>ItemXDD</li>");
+        
+    // Using WRAP
+    $('.state').wrap('<div class="US_State">/');
+    */
+
+    /*
+    // Modifying Styles
+    $('div').css({
+        'color': '#F0CF3C',
+        'font-weight': 'bold'
+    });
+    */
+
+    // Add Class 
+    //$(".classXD").addClass('bg-dark text-white');
+
+    // Remove Class
+    //$(".classXD").removeClass('bg-dark text-white');
+    
+    // HasClass
+    //console.log($(".classXD").hasClass('bg-dark')); // return false
+
+    
+
+});
